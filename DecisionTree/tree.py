c20c120aa75af07a32acd275c33d893a392d0a2f
rain <zhuyu@think-land.com>
2016-11-03 16:22:23 +0800

    决策树

---
+++ DecisionTree/tree.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+from math import log
+
+
+def create_data_set():
+    data_set = [[1, 1, 'yes'],
+                [1, 1, 'yes'],
+                [1, 0, 'no'],
+                [0, 1, 'no'],
+                [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return data_set, labels
+
+
+# 香农信息熵计算
+def calc_shannon_ent(data_set):
+    num_entries = len(data_set)
+    label_counts = {}
+    for feat_vec in data_set:
+        current_label = feat_vec[-1]
+        if current_label not in label_counts.keys():
+            label_counts[current_label] = 0
+        label_counts[current_label] += 1
+    shannon_ent = 0.0
+    for key in label_counts:
+        prob = float(label_counts[key])/num_entries
+        shannon_ent -= prob * log(prob, 2)
+    return shannon_ent
+
+
+if __name__ == '__main__':
+    my_dat, labels = create_data_set()
+    print(calc_shannon_ent(my_dat))
